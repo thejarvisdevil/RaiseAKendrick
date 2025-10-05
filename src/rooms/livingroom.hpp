15752181c846512d0d2e39e42a1970082f5d4181
jarvisdevil <jarvisdevilyt@gmail.com>
2025-10-04 19:15:39 -0500

    add todd the peashooter i forgot

--- src/rooms/livingroom.hpp
+++ src/rooms/livingroom.hpp
@@ -15,6 +15,33 @@ namespace rooms {
                 FMODAudioEngine::sharedEngine()->playEffect("tungtungtungsahurbeatupman.mp3"_spr);
             }
         }
+
+        void peashooter(CCObject*) {
+            if (parent) {
+                std::vector<std::string> messages = {
+                    "His name is Todd. Say Hi to Todd.",
+                    "He is busy killing zombies in yo lawn. Try again later.",
+                    "*barks*",
+                    "*shoots pea*",
+                    "Todd is a good peashooter.",
+                    "You should probably water Todd.",
+                    "*peashooter noises*",
+                    "Todd is the best Peashooter!!",
+                    "lol",
+                    "peashooter go brrr",
+                    "sorry, Todd is busy.",
+                    "Todd is just a peashooter",
+                    "get rekt zombies said Todd",
+                    "beware of todd",
+                    "quack quack",
+                    "meow",
+                    "woof woof",
+                    "e"
+                };
+                std::string message = messages[rand() % messages.size()];
+                FLAlertLayer::create("Peashooter", message, "OK")->show();
+            }
+        }
     };
 
     inline cocos2d::CCMenu* thelivingroom(cocos2d::CCLayer* parent = nullptr) {
@@ -30,9 +57,16 @@ namespace rooms {
         tele->setPosition({win.width / 6.5f, win.height / 2.4f});
         tele->setID("tv"_spr);
 
+        auto p = CCSprite::create("pet_peashooter.png"_spr);
+        p->setScale(0.7f);
+        auto peashooter = CCMenuItemSpriteExtra::create(p, nullptr, things, menu_selector(livingroom_stuff::peashooter));
+        peashooter->setPosition({win.width - 120.f, 60.f});
+        peashooter->setID("peashooter"_spr);
+
         auto menu = CCMenu::create();
         menu->setPosition({0,0});
         menu->addChild(tele);
+        menu->addChild(peashooter);
         menu->addChild(things);
         menu->setID("room-menu"_spr);
         return menu;
