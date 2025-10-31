1bc217ed9289316ce227a95a0d405c3943652349
FijiAura <sebastiancatania@icloud.com>
2025-10-31 12:54:28 +0800

    i had to vibe code ts 🥀

--- src/rooms/snoopland.hpp
+++ src/rooms/snoopland.hpp
@@ -5,24 +5,26 @@
 using namespace geode::prelude;
 
 namespace rooms {
+
     class snoopland_stuff : public cocos2d::CCNode {
     public:
         cocos2d::CCLayer* parent = nullptr;
+
+        // callback to play music
         void snoopdogg(CCObject*) {
             auto win = CCDirector::get()->getWinSize();
             if (parent) {
                 FMODAudioEngine::sharedEngine()->playMusic("snoopdogg.mp3"_spr, true, 1.0f, 0);
-            };
-    class snoopland_stuff {
-    public:
-        void weed(CCObject*);
+            }
+        }
+
+        // callback to show alert
+        void weed(CCObject*) {
             if (parent) {
                 FLAlertLayer::create("no", "dont do drugs", "OK")->show();
-            };
+            }
         }
     };
-
-    
 
     inline cocos2d::CCMenu* thesnoopland(cocos2d::CCLayer* parent = nullptr) {
         auto win = CCDirector::get()->getWinSize();
@@ -30,21 +32,30 @@ namespace rooms {
         things->parent = parent;
         devlin::set_emotion(parent, "excited.gif"_spr, 3.0f, 0.3f, 0.2f);
 
-        auto snoopdogg = CCMenuItemSpriteExtra::create(CCSprite::create("snoopdogg.png"_spr), nullptr, things, menu_selector(snoopland_stuff::snoopdogg));
+        auto snoopdogg = CCMenuItemSpriteExtra::create(
+            CCSprite::create("snoopdogg.png"_spr),
+            nullptr,
+            things,
+            menu_selector(snoopland_stuff::snoopdogg)
+        );
         snoopdogg->setPosition({win.width * 0.8f, win.height / 4.20f});
         snoopdogg->setID("snoopdogg"_spr);
 
-        auto weed = CCMenuItemSpriteExtra::create(CCSprite::create("weed.jpg"_spr), nullptr, things, menu_selector(snoopland_stuff::weed));
-        weed->setPosition({win.width / 12.f, win.height / 2.f});
-        weed->setID("weed"_spr);
+        auto weedItem = CCMenuItemSpriteExtra::create(
+            CCSprite::create("weed.jpg"_spr),
+            nullptr,
+            things,
+            menu_selector(snoopland_stuff::weed)
+        );
+        weedItem->setPosition({win.width / 12.f, win.height / 2.f});
+        weedItem->setID("weed"_spr);
 
         auto menu = CCMenu::create();
-        menu->setPosition({0,0});
+        menu->setPosition({0, 0});
         menu->addChild(snoopdogg);
+        menu->addChild(weedItem);
         menu->setID("room-menu"_spr);
         return menu;
     }
 
 }
-
-
